62ded02bc2def18166ebc9ab7daf9f4eba047744
evaliu <evaliu02@uw.edu>
2022-04-14 15:56:59 -0700

    Added Login Component

--- pet-my-dog-app/src/App.js
+++ pet-my-dog-app/src/App.js
@@ -1,3 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
+import { Login } from './Login.js';
+
+class AppComp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Login />
+            </div>
+        );
+    }
+}
 
