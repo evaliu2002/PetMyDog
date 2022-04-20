114d114dea34b74bcfefcaadc0fb747b3d69d759
evaliu <evaliu02@uw.edu>
2022-04-20 13:12:04 -0700

    changes to pet my dog login and signup components

--- main/front-end/src/components/Login.js
+++ main/front-end/src/components/Login.js
@@ -3,23 +3,26 @@ import React from 'react';
 export class Login extends React.Component {
     render() {
         return (
-            <div className="form">
-            <form>
-              <div className="input-container">
-                <label>Username </label>
-                <input type="text" name="uname" required />
-                {/* {renderErrorMessage("uname")} */}
-              </div>
-              <div className="input-container">
-                <label>Password </label>
-                <input type="password" name="pass" required />
-                {/* {renderErrorMessage("pass")} */}
-              </div>
-              <div className="button-container">
-                <input type="submit" />
-              </div>
-            </form>
-          </div>
+            <div>
+                <h1 className="">Pet My Dog</h1>
+                <div className="form">
+                    <form>
+                    <div className="input-container">
+                        <label> Username </label>
+                        <input type="text" placeholder="UserName" name="uname" required />
+                        {/* {renderErrorMessage("uname")} */}
+                    </div>
+                    <div className="input-container">
+                        <label> Password </label>
+                        <input type="password" placeholder="Password" name="pass" required />
+                        {/* {renderErrorMessage("pass")} */}
+                    </div>
+                    <div className="button-container">
+                        <input type="submit" />
+                    </div>
+                    </form>
+                </div>
+            </div>
         )
     }
 }
