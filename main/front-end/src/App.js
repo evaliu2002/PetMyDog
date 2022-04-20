83db72f43a50ecced30aaa617d0cd7cc6ea5998c
evaliu <evaliu02@uw.edu>
2022-04-20 13:14:55 -0700

    syntax fixes and adding signup to app

--- main/front-end/src/App.js
+++ main/front-end/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Login } from './components/Login';
+import { Signup } from './components/Signup';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Login />
+                <Signup />
             </div>
         );
     }
--- main/front-end/src/components/Signup.js
+++ main/front-end/src/components/Signup.js
@@ -31,3 +31,5 @@ export class Signup extends React.Component {
             </form>
           </div>
         )
+    }
+}
