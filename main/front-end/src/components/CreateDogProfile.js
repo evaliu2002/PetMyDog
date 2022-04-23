3d1493517f88a16ea92cc4205d199fa2b97ee474
anetw <anetw@uw.edu>
2022-04-22 19:35:15 -0700

    CreateDogProfile Form's Style Changed

--- main/front-end/src/components/CreateDogProfile.js
+++ main/front-end/src/components/CreateDogProfile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 
 export class CreateDogProfile extends React.Component {
     render() {
@@ -6,25 +7,23 @@ export class CreateDogProfile extends React.Component {
             <div>
                 <h1 className="">Your Dog's Profile</h1>
                 <div className="form">
-                    <form>
-                        <div className="input-container">
-                            <label> Dog's Name </label>
-                            <input type="text" placeholder="Enter Your Dog's Name" name="name" required />
-                        </div>
-                        <div className="input-container">
-                            <label> Dog's Breed </label>
-                            <input type="text" placeholder="Enter Your Dog's Breed" name="breed" required />
-                        </div>
-                        <div className="input-container">
-                            <label> Dog's Age </label>
-                            <input type="text" placeholder="Enter Your Dog's Age" name="age" required />
-                        </div>
-                        <div className="button-container">
-                            <button>
-                                Create Profile
-                            </button>
-                        </div>
-                    </form>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Dog's Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter Your Dog's Name" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Dog's Breed</Form.Label>
+                                <Form.Control type="text" placeholder="Your Dog's Breed Here" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                <Form.Label>Dog's Age</Form.Label>
+                                <Form.Control type="text" placeholder="Your Dog's Age Here" />
+                            </Form.Group>
+
+                        </Form>
                 </div>
             </div>
         )
