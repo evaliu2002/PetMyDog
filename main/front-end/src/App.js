9bfca11a41a7522e781fe6ce17c819b4918c367a
anetw <anetw@uw.edu>
2022-04-22 19:01:59 -0700

    CreateDogProfile Added

--- main/front-end/src/App.js
+++ main/front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Login } from './components/Login';
 import { Signup } from './components/Signup';
+import {CreateDogProfile} from './components/CreateDogProfile';
 
 class App extends React.Component {
     render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
             <div>
                 <Login />
                 <Signup />
+                <CreateDogProfile />
             </div>
         );
     }
