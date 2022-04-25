72f14b7bdc20a1a0ed81b876849a67282689fd26
evaliu <liueva2002@gmail.com>
2022-04-25 12:53:18 -0700

    Created the rest of the required implementations for the SelectedDog component

--- main/front-end/src/components/selecteddog/SelectedDog.js
+++ main/front-end/src/components/selecteddog/SelectedDog.js
@@ -4,20 +4,31 @@ export class SelectedDog extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userName: '',
-            userLocation: [0,0],
+            userLocation: {longitude: 0, latitude: 0},
             ownerName: '',
-            ownerDogName: '',
-            ownerPhoneNum: '',
-            dogLocation: [0,0],
-            distanceLeft: 0.0
+            dogName: '',
+            dogAge: 0,
+            dogBreed: '',
+            dogLocation: {longitude: 0, latitude: 0},
+            distanceLeft: 0,
         };
     }
 
     render() {
         return (
             <div>
-                
+                <h3>{ this.state.dogName }</h3>
+
+                <h5>{ this.state.dogName }'s Location </h5>
+                <p>{ this.state.distanceLeft }</p>
+
+                <h5>About { this.state.ownerDogName }</h5>
+                <p>Age: { this.state.dogAge }</p>
+                <p>Breed: { this.state.dogBreed }</p>
+
+                <img></img>
+
+                <button>Go to Dog</button>
             </div>
         );
     }
