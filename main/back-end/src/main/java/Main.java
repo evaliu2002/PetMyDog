b47513f9672a05aa644a1220958220284db05beb
wenxuan liu <30834783+PROTONlwx@users.noreply.github.com>
2022-04-25 12:21:44 -0700

    add security filter to end points and set up google oauth

--- main/back-end/src/main/java/Main.java
+++ main/back-end/src/main/java/Main.java
@@ -1,16 +1,21 @@
 import com.github.scribejava.apis.GoogleApi20;
 import org.pac4j.core.authorization.authorizer.RequireAnyRoleAuthorizer;
 import org.pac4j.core.config.Config;
+import org.pac4j.core.exception.http.HttpAction;
+import org.pac4j.core.matching.matcher.PathMatcher;
+import org.pac4j.jee.context.session.JEESessionStore;
 import org.pac4j.oidc.client.OidcClient;
 import org.pac4j.oidc.config.OidcConfiguration;
 import org.pac4j.oauth.client.Google2Client;
 import org.pac4j.oauth.client.OAuth20Client;
 import org.pac4j.oauth.config.OAuth20Configuration;
 import org.pac4j.oauth.profile.google2.Google2ProfileDefinition;
+import org.pac4j.sparkjava.CallbackRoute;
 import org.pac4j.sparkjava.SecurityFilter;
+import org.pac4j.sparkjava.SparkHttpActionAdapter;
+import org.pac4j.sparkjava.SparkWebContext;
 import spark.template.mustache.MustacheTemplateEngine;
-import org.pac4j.jee.context.session.JEESessionStore;
-
+import org.pac4j.core.context.session.SessionStore;
 
 
 import java.util.Optional;
@@ -20,27 +25,68 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        /*****************************************     Begin OAuth config     *****************************************/
+
+        // Setup google oauth api configuration with pac4j
         final OidcConfiguration oidcConfiguration = new OidcConfiguration();
         oidcConfiguration.setClientId("215324184605-v64ebk4kddt6aufdkmbaf0iv38doeqdb.apps.googleusercontent.com");
         oidcConfiguration.setSecret("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");
         oidcConfiguration.setDiscoveryURI("https://accounts.google.com/.well-known/openid-configuration");
         oidcConfiguration.setUseNonce(true);
-
         oidcConfiguration.addCustomParam("prompt", "consent");
+
+        // Create client with the configuration
         final OidcClient oidcClient = new OidcClient(oidcConfiguration);
         oidcClient.setAuthorizationGenerator((ctx, session, profile) -> {
             profile.addRole("ROLE_ADMIN");
             return Optional.of(profile);
         });
+        oidcClient.setCallbackUrl("http://localhost:4567/callback");
 
+        // Security configuration using google client
         Config config = new Config(oidcClient);
-        config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("ROLE_ADMIN"));
         config.addAuthorizer("custom", new CustomAuthorizer());
-        config.setHttpActionAdapter(new DemoHttpActionAdapter(new MustacheTemplateEngine()));
+        config.setHttpActionAdapter(new DemoHttpActionAdapter());
+
+        // Set up call back end points
+        CallbackRoute callback = new CallbackRoute(config, "/hello", true);
+        get("/callback", callback);
+        post("/callback", callback);
+
+        /*******************************************     End OAuth config     *****************************************/
+
+
+
+        /*******************************************   Start Security Guard   *****************************************/
 
         before("/hello", new SecurityFilter(config, "GoogleClient"));
+
+        /******************************************    End Security Guard     *****************************************/
+
+
+
+        /****************************************   Start Service end pints   *****************************************/
+
         get("/hello", (req, res) -> "Hello World");
-        get("/callback", (req, res) -> "Hello World again");
+        get("/login", (req, res) -> {
+            final SparkWebContext context = new SparkWebContext(req, res);
+            HttpAction action;
+            try {
+                action = oidcClient.getRedirectionAction(context, JEESessionStore.INSTANCE).get();
+            } catch (final HttpAction e) {
+                action = e;
+            }
+            SparkHttpActionAdapter.INSTANCE.adapt(action, context);
+            return null;
+        });
+
+        /******************************************   End Service end pints   *****************************************/
+
+
+        /****************************************   Start utils   *****************************************/
+
+        /****************************************   End utils   *****************************************/
 
     }
 }
