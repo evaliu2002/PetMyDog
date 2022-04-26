2a671e63f64d50e86f1e712a9fd50302d3759f28
evaliu <liueva2002@gmail.com>
2022-04-26 02:04:54 -0700

    Started on google login feature

--- main/front-end/src/components/login/Login.js
+++ main/front-end/src/components/login/Login.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 
 export class Login extends React.Component {
     render() {
+        const handleFailure = (result) => {
+            alert(result);
+        };
+        const handleLogin = (googleData) => {
+            console.log(googleData);
+        }
         return (
             <div>
                 <h1 className="">Pet My Dog</h1>
-                <div className="form">
+                {/* <div className="form">
                     <form>
                     <div className="input-container">
                         <label> Username </label>
@@ -19,7 +26,14 @@ export class Login extends React.Component {
                         <input type="submit" />
                     </div>
                     </form>
-                </div>
+                </div> */}
+                <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                buttonText="Log in with Google"
+                onSuccess={handleLogin}
+                onFailure={handleFailure}
+                cookiePolicy={'single_host_origin'}
+                ></GoogleLogin>
             </div>
         )
     }
