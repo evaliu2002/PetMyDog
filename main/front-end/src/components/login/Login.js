0bb8364f260272bbe89cca3c4c7740ceb672585b
evaliu <liueva2002@gmail.com>
2022-04-26 13:51:14 -0700

    Deleted old login form code

--- main/front-end/src/components/login/Login.js
+++ main/front-end/src/components/login/Login.js
@@ -12,21 +12,6 @@ export class Login extends React.Component {
         return (
             <div>
                 <h1 className="">Pet My Dog</h1>
-                {/* <div className="form">
-                    <form>
-                    <div className="input-container">
-                        <label> Username </label>
-                        <input type="text" placeholder="UserName" name="uname" required />
-                    </div>
-                    <div className="input-container">
-                        <label> Password </label>
-                        <input type="password" placeholder="Password" name="pass" required />
-                    </div>
-                    <div className="button-container">
-                        <input type="submit" />
-                    </div>
-                    </form>
-                </div> */}
                 <GoogleLogin
                 clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
                 buttonText="Log in with Google"
