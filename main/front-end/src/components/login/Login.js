93fb674b3fc43180702b1e8ffe8ae0bfa67641f9
evaliu <liueva2002@gmail.com>
2022-04-26 22:28:40 -0700

    Progress in login component

--- main/front-end/src/components/login/Login.js
+++ main/front-end/src/components/login/Login.js
@@ -1,24 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleLogin from 'react-google-login';
 
 export class Login extends React.Component {
-    render() {
+    function Login() {
+        const [loginData, setLoginData] = useState(
+            localStorage.getItem('loginData')
+            ? JSON.parse(loginData.getItem('loginData'))
+            : null
+        );
+
         const handleFailure = (result) => {
             alert(result);
         };
-        const handleLogin = (googleData) => {
-            console.log(googleData);
-        }
+
+        const handleLogin = async (googleData) => {
+            const res = await fetch('/api/google-login', {
+                method: 'POST',
+                body: JSON.stringify({
+                    token: googleData.tokenId,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            const data = await res.json();
+            setLoginData(data);
+            localStorage.setItem('loginData', JSON.stringify(data));
+            
+        };
+
+        const handleLogout = () => {
+            localStorage.removeItem('loginData');
+            setLoginData(null);
+        
+        };
+
         return (
-            <div>
-                <h1 className="">Pet My Dog</h1>
-                <GoogleLogin
-                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                buttonText="Log in with Google"
-                onSuccess={handleLogin}
-                onFailure={handleFailure}
-                cookiePolicy={'single_host_origin'}
-                ></GoogleLogin>
+            <div className='Login'>
+                <header className='Login-header'>
+                    <div>
+                        {
+                            loginData ? (
+                                <div>
+                                <h3>You logged in as {loginData.email}</h3>
+                                <button onClick={handleLogout}>Logout</button>
+                                </div>
+                            ) : (
+                                <GoogleLogin
+                                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                                    buttonText="Log in with Google"
+                                    onSuccess={handleLogin}
+                                    onFailure={handleFailure}
+                                    cookiePolicy={'single_host_origin'}
+                                ></GoogleLogin>
+                            )}
+                        <h1 className="">Pet My Dog</h1>
+                    </div>
+                </header>
             </div>
         )
     }
