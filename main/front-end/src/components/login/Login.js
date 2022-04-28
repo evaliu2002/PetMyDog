c235744e48626974c3839bfed9aec86f4dd2ce31
evaliu <liueva2002@gmail.com>
2022-04-28 00:45:35 -0700

    Google Login, will update implementation once I get a look at back end

--- main/front-end/src/components/login/Login.js
+++ main/front-end/src/components/login/Login.js
@@ -1,64 +1,63 @@
 import React, { useState } from 'react';
 import GoogleLogin from 'react-google-login';
+import { useHistory } from "react-router-dom";
 
-export class Login extends React.Component {
-    function Login() {
-        const [loginData, setLoginData] = useState(
-            localStorage.getItem('loginData')
-            ? JSON.parse(loginData.getItem('loginData'))
-            : null
-        );
+function Login() {
+    const [loginData, setLoginData] = useState(
+        localStorage.getItem('loginData')
+        ? JSON.parse(loginData.getItem('loginData'))
+        : null
+    );
 
-        const handleFailure = (result) => {
-            alert(result);
-        };
+    const handleFailure = (result) => {
+        alert(result);
+    };
 
-        const handleLogin = async (googleData) => {
-            const res = await fetch('/api/google-login', {
-                method: 'POST',
-                body: JSON.stringify({
-                    token: googleData.tokenId,
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
+    const handleLogin = async (googleData) => {
+        const res = await fetch('/api/google-login', {
+            method: 'POST',
+            body: JSON.stringify({
+                token: googleData.tokenId,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
 
-            const data = await res.json();
-            setLoginData(data);
-            localStorage.setItem('loginData', JSON.stringify(data));
-            
-        };
-
-        const handleLogout = () => {
-            localStorage.removeItem('loginData');
-            setLoginData(null);
+        const data = await res.json();
+        setLoginData(data);
+        localStorage.setItem('loginData', JSON.stringify(data));
         
-        };
+    };
 
-        return (
-            <div className='Login'>
-                <header className='Login-header'>
-                    <div>
-                        {
-                            loginData ? (
-                                <div>
-                                <h3>You logged in as {loginData.email}</h3>
-                                <button onClick={handleLogout}>Logout</button>
-                                </div>
-                            ) : (
-                                <GoogleLogin
-                                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                                    buttonText="Log in with Google"
-                                    onSuccess={handleLogin}
-                                    onFailure={handleFailure}
-                                    cookiePolicy={'single_host_origin'}
-                                ></GoogleLogin>
-                            )}
-                        <h1 className="">Pet My Dog</h1>
-                    </div>
-                </header>
-            </div>
-        )
-    }
+    const handleLogout = () => {
+        localStorage.removeItem('loginData');
+        setLoginData(null);
+    
+    };
+
+    return (
+        <div className='Login'>
+            <header className='Login-header'>
+                <div>
+                    {
+                        loginData ? (
+                            <div>
+                            <h3>You logged in as {loginData.email}</h3>
+                            <button onClick={handleLogout}>Logout</button>
+                            </div>
+                        ) : (
+                            <GoogleLogin
+                                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                                buttonText="Log in with Google"
+                                onSuccess={handleLogin}
+                                onFailure={handleFailure}
+                                cookiePolicy={'single_host_origin'}
+                            ></GoogleLogin>
+                        )}
+                    <h1 className="">Pet My Dog</h1>
+                </div>
+            </header>
+        </div>
+    )
 }
