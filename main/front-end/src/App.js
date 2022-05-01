3a16bb6b25ecff688fe7bf5782d09b9ee894a2e4
evaliu <liueva2002@gmail.com>
2022-04-30 18:24:43 -0700

    slight path change to Login route in App

--- main/front-end/src/App.js
+++ main/front-end/src/App.js
@@ -6,9 +6,9 @@ import {Route, Routes} from "react-router";
 const App = () => {
     return (
         <div id='App'>
-            <Login/>
+            <Login />
             <Routes>
-                <Route path="http://localhost:3001" element={<Login />} />
+                <Route path="http://localhost:3000" element={<Login />} />
                 <Route path="main/front-end/src/components/finddogs/FindDogs.js" element={<FindDogs />}/>
             </Routes>
         </div>
