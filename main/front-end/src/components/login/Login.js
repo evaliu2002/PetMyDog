756d7b1ce97ebc1c4abfb05cd6a42b0f954d9f38
evaliu <liueva2002@gmail.com>
2022-05-10 14:58:01 -0700

    change color of h1

--- main/front-end/src/components/login/Login.js
+++ main/front-end/src/components/login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
 
     return (
         <div className='login'>
-            <h1 className='font-link'>Pet My Dog</h1>
+            <h1 className='font-link' style={{color: "#E5737D"}}>Pet My Dog</h1>
             <a className="btn btn-outline-dark" href="http://petmydog.fun/login" role="button" style={{textTransform: "none"}}>
                 <img width="20px" style={{marginBlock: "3px", marginRight: "5px"}} alt="Google sign-in"
                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"/>
