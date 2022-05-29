dbb6285735bb6d902cf90fd7b02c16b0da7b79f6
wenxuan liu <30834783+PROTONlwx@users.noreply.github.com>
2022-05-28 22:31:38 -0700

    add test

--- main/front-end/src/App.test.js
+++ main/front-end/src/App.test.js
@@ -4,6 +4,24 @@ import {BrowserRouter} from "react-router-dom";
 import React from 'react';
 import {Circle, initialize, Map, Marker, mockInstances} from "@googlemaps/jest-mocks";
 import { useAuth0 } from "@auth0/auth0-react";
+import FindDogs from "./components/FindDogs";
+import {shallow} from 'enzyme';
+
+
+// Clear all mocks
+beforeEach(() => {
+    mockInstances.clearAll();
+});
+
+// Clear specific mocks
+beforeEach(() => {
+    mockInstances.clear(Map, Marker);
+});
+
+
+afterAll(() => {
+    global.fetch = unmockedFetch
+})
 
 /**
  * Testing Pet My Dog Login Heading Display
@@ -80,16 +98,6 @@ test('mocking-google-maps', () => {
     expect(circleMocks[0].setMap).toHaveBeenCalledTimes(1);
 });
 
-// Clear all mocks
-beforeEach(() => {
-    mockInstances.clearAll();
-});
-
-// Clear specific mocks
-beforeEach(() => {
-    mockInstances.clear(Map, Marker);
-});
-
 /**
  * Testing Status Change of Meeting Requests after requests are
  * accepted or declined.
@@ -97,3 +105,36 @@ beforeEach(() => {
 test('request-status-change', () => {
 
 });
+
+const unmockedFetch = global.fetch
+
+test('nearby-dogs', () => {
+    const dog1 = {"did":"9","name":"dog1","age":"7","gender":"male","breed":"test4","pic_link":"test"}
+    const dog2 = {"did":"9","name":"dog2","age":"7","gender":"male","breed":"test4","pic_link":"test"}
+    const dog3 = {"did":"9","name":"dog3","age":"7","gender":"male","breed":"test4","pic_link":"test"}
+    const user1 = {"uid":"0","username":"k","email":"f","pic_link":"s","last_ping":"1","dogs":[dog1,dog2]}
+    const user2 = {"uid":"1","username":"l","email":"f","pic_link":"s","last_ping":"1","dogs":[dog3]}
+    const user3 = {"uid":"2","username":"l","email":"f","pic_link":"s","last_ping":"1","dogs":[]}
+    const mockFetch = jest.fn()
+        .mockReturnValueOnce({
+            json: () => Promise.resolve([0, 1, 2]),
+        })
+        .mockReturnValueOnce({
+            json: () => Promise.resolve(user1)
+        })
+        .mockReturnValueOnce({
+            json: () => Promise.resolve(user2)
+        })
+        .mockReturnValueOnce({
+            json: () => Promise.resolve(user3)
+        })
+    global.fetch = mockFetch
+
+    const wrapper = shallow(
+        <FindDogs />,
+    );
+    wrapper.instance().updateNearbyUsers();
+
+})
+
+
